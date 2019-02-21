96be2588b4e31cc25b410327115ee5a1394222c5
Asim Zaidi <asimzaidih@gmail.com>
2019-02-21 14:13:17 -0800

    basic testing suite example

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,17 +1,19 @@
 package raindrops
+
 import (
 	"strconv"
 )
 
-func Convert(num int) (string) {
+// Convert func does converting stuff
+func Convert(num int) string {
 	var output = ""
-	if num % 3 == 0 {
+	if num%3 == 0 {
 		output += "Pling"
 	}
-	if num % 5 == 0 {
-		output += "Plang" 
+	if num%5 == 0 {
+		output += "Plang"
 	}
-	if num % 7 == 0{
+	if num%7 == 0 {
 		output += "Plong"
 	}
 	if output == "" {
@@ -21,5 +23,5 @@ func Convert(num int) (string) {
 }
 
 // Notes:
-	// When you write in Go, especially when writing if's the compiler adds a semicolon
-	// when every 'line' finishes so you have to have them on the same line!
+// When you write in Go, especially when writing if's the compiler adds a semicolon
+// when every 'line' finishes so you have to have them on the same line!
